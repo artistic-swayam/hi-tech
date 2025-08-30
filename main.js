fb22aa5c582defe81a9deec34f7b160dc7f719b7
Swayam <152770645+artistic-swayam@users.noreply.github.com>
2025-08-30 05:19:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,96 @@
-function menu(){
+
+
+function menu() {
   const openBtn = document.querySelector(".open");
-const closeBtn = document.querySelector(".close");
-const menu = document.querySelector(".menu");
+  const closeBtn = document.querySelector(".close");
+  const menu = document.querySelector(".menu");
 
-openBtn.addEventListener("click",()=>{
-    gsap.to(".menu",{
-        y:0
-    })
-})
-
-menu.addEventListener("click",()=>{
-  gsap.to(menu, { 
-    x: "-100%", 
-
-    duration: 0.6, 
-    ease: "power3.in",
-    onComplete: () => { 
-      menu.style.display = "none"; 
-      closeBtn.style.display = "none"; // hide close button only after animation
-      openBtn.style.display = "block"; 
-    }
+  openBtn.addEventListener("click", () => {
+    menu.style.display = "block"; // show it first
+    gsap.fromTo(menu,
+      { y: "-100%" },
+      { y: "0%", duration: .6, ease: "power3.out" }
+    );
+    gsap.from(closeBtn, {
+      rotate: "180deg",
+      duration: 1.2, ease: "power3.out"
+    });
+    gsap.from(openBtn, {
+      rotate: "180deg",
+      duration: 1.2, ease: "power3.out"
+    });
   });
-})
+
+  // CLOSE
+  closeBtn.addEventListener("click", () => {
+    gsap.to(menu, {
+      y: "-100%",
+
+      duration: 0.5,
+      ease: "power3.in",
+      onComplete: () => {
+        menu.style.display = "none"; // hide after animation
+      }
+    });
+    gsap.from(closeBtn, {
+      rotate: "90deg",
+      duration: 1.2, ease: "power3.out"
+    })
+  });
+
+
+}
+function about(){
+  gsap.registerPlugin(ScrollTrigger);
+  
+  gsap.from(".up",{
+    opacity:0,
+    duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: ".up",
+          start: "top 80%", scrub:true,
+          toggleActions: "play none none reverse"
+        },onComplete:()=>{
+          gsap.utils.toArray(".left").forEach((el) => {
+    gsap.fromTo(el,
+      { x: -100, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: el,
+          start: "top 80%", scrub:true,
+          toggleActions: "play none none reverse"
+        }
+      }
+    );
+  });
+
+  // Animate .right
+  gsap.utils.toArray(".right").forEach((el) => {
+    gsap.fromTo(el,
+      { x: 100, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: el,
+          start: "top 80%", scrub:true,
+          toggleActions: "play none none reverse"
+        }
+      }
+    );
+  });
+        }
+  })
+
+  
 }
 
 menu();
+about();
