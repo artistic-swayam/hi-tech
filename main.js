0dac50aa9e5e0b89738b2dfbd91a1be63b2598fe
Swayam <152770645+artistic-swayam@users.noreply.github.com>
2025-09-05 04:07:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
 
 
-// function menu() {
-//   const openBtn = document.querySelector(".open");
-//   const closeBtn = document.querySelector(".close");
-//   const menu = document.querySelector(".menu");
+function menu() {
+  const openBtn = document.querySelector(".open");
+  const closeBtn = document.querySelector(".close");
+  const menu = document.querySelector(".menu");
 
-//   openBtn.addEventListener("click", () => {
-//     menu.style.display = "block"; // show it first
-//     gsap.fromTo(menu,
-//       { y: "-100%" },
-//       { y: "0%", duration: .6, ease: "power3.out" }
-//     );
-//     gsap.from(closeBtn, {
-//       rotate: "180deg",
-//       duration: 1.2, ease: "power3.out"
-//     });
-//     gsap.from(openBtn, {
-//       rotate: "180deg",
-//       duration: 1.2, ease: "power3.out"
-//     });
-//   });
+  openBtn.addEventListener("click", () => {
+    menu.style.display = "block"; // show it first
+    gsap.fromTo(menu,
+      { y: "-100%" },
+      { y: "0%", duration: .6, ease: "power3.out" }
+    );
+    gsap.from(closeBtn, {
+      rotate: "180deg",
+      duration: 1.2, ease: "power3.out"
+    });
+    gsap.from(openBtn, {
+      rotate: "180deg",
+      duration: 1.2, ease: "power3.out"
+    });
+  });
 
-//   // CLOSE
-//   closeBtn.addEventListener("click", () => {
-//     gsap.to(menu, {
-//       y: "-100%",
+  // CLOSE
+  closeBtn.addEventListener("click", () => {
+    gsap.to(menu, {
+      y: "-100%",
 
-//       duration: 0.5,
-//       ease: "power3.in",
-//       onComplete: () => {
-//         menu.style.display = "none"; // hide after animation
-//       }
-//     });
-//     gsap.from(closeBtn, {
-//       rotate: "90deg",
-//       duration: 1.2, ease: "power3.out"
-//     })
-//   });
-// }
+      duration: 0.5,
+      ease: "power3.in",
+      onComplete: () => {
+        menu.style.display = "none"; // hide after animation
+      }
+    });
+    gsap.from(closeBtn, {
+      rotate: "90deg",
+      duration: 1.2, ease: "power3.out"
+    })
+  });
+}
 
 
 
