a07067b10ac139d03517e77ecf67e90f7e533a32
Swayam <152770645+artistic-swayam@users.noreply.github.com>
2025-09-14 05:13:12 -0700

    Update main.js

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ document.querySelectorAll(".split-others").forEach(el => {
   });
 });
 
-  
+
 }
 
 
@@ -170,11 +170,34 @@ function lenis() {
   requestAnimationFrame(raf);
 }
 
+function show(){
+const btn = document.querySelector(".show-toggle");
+const works = document.querySelectorAll(".w");
+const workSection = document.querySelector(".work");
+
+btn.addEventListener("click", () => {
+  const isHidden = works[0].classList.contains("no-w");
+
+  works.forEach(w => {
+    w.classList.toggle("no-w", !isHidden);
+  });
+
+  // toggle the whole .work height
+  workSection.classList.toggle("expanded", isHidden);
+
+  // update button text
+  btn.textContent = isHidden ? "Show Less" : "Show More";
+});
+
+}
+
 lenis();
 menu();
 about();
 text();
 image();
+show();
+
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -184,7 +207,7 @@ cards.forEach((card,i)=>{
   gsap.to(card,{
     scale: 0.8+0.2*(i/(cards.length-1)),
     ease:"none",
-    
+
     scrollTrigger:{
       trigger:card,
       start:"-50%",
@@ -197,5 +220,149 @@ cards.forEach((card,i)=>{
 
     }
   })
-  
+
 })
+
+
+const carousels = {
+  w1: [
+    "url('f1.jpg')",
+    "url('f2.jpg')",
+
+  ],
+  w2: [
+    "url('f3.jpg')",
+    "url('f38.JPG')"
+  ],
+  w3: [
+    "url('f4.JPG')",
+    "url('f45.jpg')",
+    "url('f46.jpg')",
+    // "url('f38.JPG')"
+  ],
+  w4: [
+    "url('f5.jpg')",
+    "url('f13.JPG')"
+  ],
+  w5: [
+    "url('f6.jpeg')",
+    "url('f7.jpeg')",
+    "url('f39.JPG')",
+    "url('f54.jpeg')"
+  ],
+  w6: [
+    "url('f40.JPG)",
+
+  ],
+  w7: [
+    "url('f12.jpg')",
+    "url('f35.JPG')",
+    "url('f36.JPG')"
+    //"url('f38.JPG')"
+  ],
+  // w8: [
+  //   "url('f13.jpg')",
+  //   //"url('f38.JPG')"
+  // ],
+  w9: [
+    "url('f14.jpeg')",
+    "url('f15.jpeg')",
+    "url('f47.jpg')"
+  ],
+  w10: [
+    "url('f16.jpeg')",
+    "url('f17.jpeg')"
+  ],
+  w11: [
+    "url('f18.jpg')",
+    "url('f19.jpg')"
+  ],
+  w12: [
+    "url('f20.jpg')",
+    "url('f21.jpg')",
+    "url('f22.jpg')",
+    "url('f23.jpg')",
+    "url('f24.jpg')",
+    "url('f25.jpg')",
+  ],
+  w13: [
+    "url('f26.jpg')",
+    "url('f31.JPG')"
+  ],
+  w14: [
+    "url('f29.JPG')",
+    "url('f30.JPG')",
+    "url('f42.jpg')",
+    "url('f43.jpg')",
+  ],
+  w15: [
+    "url('f32.JPG')",
+    //"url('f38.JPG')"
+  ],
+   w16: [
+    "url('f33.JPG')",
+    "url('f44.jpg')"
+  ],
+   w17: [
+    "url('f34.JPG')",
+    //"url('f38.JPG')"
+  ],
+   w18: [
+    //"url('f3.jpg')",
+    "url('f48.jpg')"
+  ],
+  //  w19: [
+    
+  // ],
+   w20: [
+    "url('f27.jpg')",
+    "url('f28.jpg')",
+    "url('f41.jpg')",
+  ],
+  //  w21: [
+    
+    
+  // ],
+   w22: [
+    //"url('f3.jpg')",
+    "url('f49.jpg')"
+  ],
+   w23: [
+    "url('f50.jpeg')",
+    //"url('f38.JPG')"
+  ],
+   w24: [
+    "url('f55.jpeg')",
+
+  ],
+   w25: [
+    "url('f57.jpg')",
+    //"url('f38.JPG')"
+  ],
+  //  w26: [
+  //   "url('f3.jpg')",
+  //   "url('f38.JPG')"
+  // ],
+  //  w27: [
+  //   "url('f3.jpg')",
+  //   "url('f38.JPG')"
+  // ],
+  //  w28: [
+  //   "url('f3.jpg')",
+  //   "url('f38.JPG')"
+  // ],
+  //  w29: [
+  //   "url('f3.jpg')",
+  //   "url('f38.JPG')"
+  // ],
+};
+
+Object.keys(carousels).forEach(id => {
+  const div = document.getElementById(id);
+  let i = 0;
+
+  setInterval(() => {
+    div.style.backgroundImage = carousels[id][i];
+    i = (i + 1) % carousels[id].length;
+  }, 3000); // every 3 sec
+});
